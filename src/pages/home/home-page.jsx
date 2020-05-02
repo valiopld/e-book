1fac289ee3c1455bb039020f86a6531d7c8f8f19
Valentin <valiopld@gmail.com>
2020-05-02 19:26:00 +0300

    Created Home and Search Page

---
+++ src/pages/home/home-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./search-page.css";
+
+class SearchPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div className="search-page">This is Search Page</div>;
+  }
+}
+export default SearchPage;
---
+++ src/pages/search/search-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./home-page.css";
+
+class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div className="home-page">This is Home Page</div>;
+  }
+}
+export default HomePage;
